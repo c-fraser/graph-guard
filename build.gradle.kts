e6fa5e434a0d394eb1d5b4dcb933257df6814840
c-fraser <cfraser888@gmail.com>
2024-02-23 09:37:59 -0500

    changed: bumped version

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ allprojects project@{
   apply(plugin = "org.jetbrains.kotlin.jvm")
 
   group = "io.github.c-fraser"
-  version = "0.8.1"
+  version = "0.8.2"
 
   configure<JavaPluginExtension> {
     toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
